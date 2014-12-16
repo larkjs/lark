3b8c41eeafb37ad141aa90b97e991efdac535973
mdemo <mengdesen09@qq.com>
2014-12-16 16:16:14 +0800

    add test and travis

---
+++ test/application.test.js
@@ -0,0 +1,27 @@
+/**
+ * lark - application.test.js.js
+ * Copyright(c) 2014 larkjs-team(https://github.com/larkjs)
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var request = require('supertest');
+var lark = require('../lib/application');
+
+describe('lib/application.js', function () {
+  describe('app instance', function(){
+    it('app instanceof lark should equal true', function(done){
+      var app = lark({
+        directory: "example/configs"
+      });
+      (app instanceof lark).should.equal(true);
+      done();
+    })
+  })
+});
+
