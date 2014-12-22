f0c00569cedeae8651b76b58c456b8d9e8871775
mdemo <mengdesen09@qq.com>
2014-12-22 15:52:32 +0800

    fix

--- example/models/pageServices/list.js
+++ example/models/pageServices/list.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 var pageService = require('lark-mvc').pageService;
 var demo = pageService.create('demo');
-=======
-var pageService = require('lark-mvc').pageService
-var demo = pageService.create('demo')
->>>>>>> master
 demo.render = function () {
   return this.dataServices.demo.getData() + '-pageService'
 };
