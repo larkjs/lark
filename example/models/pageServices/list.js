1c245c739e515ef7e74ba4b382d5cfc195c66219
mdemo <mengdesen09@qq.com>
2015-03-11 19:11:54 +0800

    为 lark-mvc 传入 app 内容

--- example/models/pageServices/list.js
+++ example/models/pageServices/list.js
@@ -1,7 +1,7 @@
-
-module.exports = function(mvc){
-  var demo = mvc.pageService.create('demo');
-  demo.render = function(){
-    return this.dataServices.demo.getData() + '-pageService'
-  }
+module.exports = function (mvc, lark) {
+    var demo = mvc.pageService.create('demo');
+    demo.render = function () {
+        lark.log.info(123);
+        return this.dataServices.demo.getData() + '-pageService'
+    }
 };
